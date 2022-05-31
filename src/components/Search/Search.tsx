2acdc9389c9f008f12e0be2d25ac8cbcc7cce399
Joel Chuca <joelchuca.dev@hotmail.com>
2022-05-31 11:49:24 -0400

    implement search functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,30 +1,46 @@
 import * as React from 'react';
+import axios from "axios";
 
-export interface ISearchProps {
-}
+export default function Search () {
+  const [imageName, setImageName] = React.useState("ditto");
+  const [isShiny, setIsShiny] = React.useState(true);
+  const [imageUrl, setImageUrl] = React.useState("");
 
-export default function Search (props: ISearchProps) {
-  const [imageName, setImageName] = React.useState("");
-  const [imageType, setImageType] = React.useState("");
-
-  const onSearch = ( event: any)  => {
-    console.log(event.target.value);
+  const processImage = (data: any) => {
+    console.log(data);
     
+    if(!data.sprites) {
+       alert("Image not found");
+       return;
+    }
+
+    console.log(isShiny ? data.sprites.front_shiny : data.sprites.front_dafault);
+    setImageUrl(isShiny ? data.sprites.front_shiny : data.sprites.front_dafault);
+  }
+  
+  const onSearch = ( event: any)  => {
+    setImageUrl("");
+    const url = "https://pokeapi.co/api/v2/pokemon/";
+    axios.get(`${url + imageName}`)
+      .then((response) => {
+        processImage(response.data)
+      })
+      .catch(err => { 
+        console.log(err); 
+        alert("Image not found")
+      });
   }
 
   const onName = ( event: any) => {
-    console.log(event.target.value);
     setImageName(event.target.value);
   }
 
   const onType = ( event: any) => {
-    console.log(event.target.checked);
-    setImageType(event.target.value);
+    setIsShiny(event.target.checked);
   }
 
   const onAdd = ( event: any)  => {
-    console.log(event.target.value);
-    
+    console.log(event.target.value);    
   }
 
   return (
@@ -33,14 +49,16 @@ export default function Search (props: ISearchProps) {
         <label htmlFor="imageName">Image Name</label>
         <input type="search" id="imageName" value={imageName} onChange={onName}/>
         <label htmlFor="imageType">Shiny</label>
-        <input type="checkbox" id="imageType" value="shiny" onChange={onType}/>
+        <input type="checkbox" id="imageType" checked={isShiny} onChange={onType}/>
         <input type="button" value="Search" onClick={onSearch}></input>
       <div>
   
       </div>
-        <img src = ""
-          alt = {imageName} height = "250" width = "270" />
-        <input type="button" value="Add" onClick={onAdd}></input>
+        {imageUrl !== "" && 
+        <>
+          <img src = {imageUrl} alt = {imageName}  /> 
+          <input type="button" value="Add" onClick={onAdd}></input> 
+        </>}
       </div>
 
     </div>
